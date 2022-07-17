dc6e10da8c73aecb01e6a40ae7a4d9537ef123d9
25081997pc <chavanpooja2508@gmail.com>
2022-07-17 17:49:37 +0530

    Printing Fibonacci Series

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Solve Logical Programs");
+            int n1 = 0, n2 = 1, n3, num;
+
+            Console.WriteLine("Enter the number of Elements: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(n1 + " " + n2 + " ");
+
+            for(int i = 2; i < num; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+
+                n1 = n2;
+                n2 = n3;
+            }
         }
     }
 }
