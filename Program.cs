ce3d7dec613f5ab984349aec5165af16e0624368
25081997pc <chavanpooja2508@gmail.com>
2022-07-17 19:36:57 +0530

    Printing Reverse Number

--- Program.cs
+++ Program.cs
@@ -4,37 +4,18 @@
     {
         static void Main(string[] args)
         {
-            int n, i, flag = 0;
-            int min, max;
+            int num, reverse = 0, remainder = 0;
 
-            Console.WriteLine("Find the Prime number within a given range of number");
+            Console.WriteLine("Enter the Number : ");
+            num = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Input the starting number of range:");
-            min = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Input the Ending number of range:");
-            max = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Prime numbers within the given range");
-            if(min >= 2)
+            while (num != 0)
             {
-                for(n = min; n <= max; n++)
-                {
-                    i = 2;
-                    flag = 0;
-                    while(i <= n/2)
-                    {
-                        if (n % i == 0)
-                        {
-                            flag = 1;
-                            break;
-                        }
-                        i = i + 1; 
-                    }
-                    if(flag == 0)
-                        Console.Write("{0} ", n);
-                }
+                remainder = num % 10;
+                reverse = reverse * 10 + remainder;
+                num = num / 10;
             }
+            Console.WriteLine("Reverse Number is : " + reverse);
         }
     }
 }
